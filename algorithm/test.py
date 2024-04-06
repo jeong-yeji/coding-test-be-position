4de9b0007d93c29852b694c7644ca351f0663dcb
jeong-yeji <jeongyeji.mail@gmail.com>
2024-04-06 16:15:28 +0900

    test: deque를 이용한 풀이를 위한 테스트 코드 작성

--- algorithm/test.py
+++ algorithm/test.py
@@ -1,5 +1,7 @@
 import unittest
-import sol1
+from collections import deque
+
+import sol1, sol2
 from parameterized import parameterized
 
 
@@ -11,7 +13,7 @@ class AntSequenceTest(unittest.TestCase):
         (7, 12),
         (8, 21),
     ])
-    def test_solution(self, n, result):
+    def test_for_sol1(self, n, result):
         self.assertEqual(sol1.solution(n), result)
 
     @parameterized.expand([
@@ -24,15 +26,45 @@ class AntSequenceTest(unittest.TestCase):
         (7, [1], [1, 3, 1, 1, 2, 2, 2, 1]),
         (8, [1], [1, 1, 1, 3, 2, 1, 3, 2, 1, 1]),
     ])
-    def test_make_ant_sequence(self, n, ln, result):
+    def test_make_ant_sequence_for_sol1(self, n, ln, result):
         self.assertEqual(sol1.make_ant_sequence(n, ln), result)
 
-    def test_value_error(self):
+    def test_value_error_for_sol1(self):
         with self.assertRaises(ValueError):
             sol1.solution(3)
 
         with self.assertRaises(ValueError):
             sol1.solution(100)
+
+    @parameterized.expand([
+        (4, 21),
+        (5, 12),
+        (6, 22),
+        (7, 12),
+        (8, 21),
+    ])
+    def test_for_sol2(self, n, result):
+        self.assertEqual(sol2.solution(n), result)
+
+    @parameterized.expand([
+        (1, deque([1]), deque([1])),
+        (2, deque([1]), deque([1, 1])),
+        (3, deque([1]), deque([2, 1])),
+        (4, deque([1]), deque([1, 2, 1, 1])),
+        (5, deque([1]), deque([1, 1, 1, 2, 2, 1])),
+        (6, deque([1]), deque([3, 1, 2, 2, 1, 1])),
+        (7, deque([1]), deque([1, 3, 1, 1, 2, 2, 2, 1])),
+        (8, deque([1]), deque([1, 1, 1, 3, 2, 1, 3, 2, 1, 1])),
+    ])
+    def test_make_ant_sequence_for_sol2(self, n, ln, result):
+        self.assertEqual(sol2.make_ant_sequence(n, ln), result)
+
+    def test_value_error_for_sol2(self):
+        with self.assertRaises(ValueError):
+            sol2.solution(3)
+
+        with self.assertRaises(ValueError):
+            sol2.solution(100)
 
 
 if __name__ == '__main__':
