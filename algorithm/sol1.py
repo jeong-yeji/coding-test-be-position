a89266cff1e5d06398b82724aad1ed1e6c6cd1ba
jeong-yeji <jeongyeji.mail@gmail.com>
2024-04-06 15:59:36 +0900

    test: 테스트 코드 파일 분리

---
+++ algorithm/sol1.py
@@ -0,0 +1,27 @@
+def solution(n: int) -> int:
+    ant_sequence = make_ant_sequence(n, [1])
+    mid = len(ant_sequence) // 2
+    return ant_sequence[mid - 1] * 10 + ant_sequence[mid]
+
+
+def make_ant_sequence(n: int, ln: list) -> list:
+    if n == 1:
+        return ln
+
+    cur, cnt = ln[0], 1
+    new_ln = []
+    for i in range(1, len(ln)):
+        if cur == ln[i]:
+            cnt += 1
+        else:
+            new_ln.append(cnt)
+            new_ln.append(cur)
+            cur, cnt = ln[i], 1
+    new_ln.append(cnt)
+    new_ln.append(cur)
+
+    return make_ant_sequence(n - 1, new_ln)
+
+
+if __name__ == "__main__":
+    print(solution(int(input())))
--- algorithm/test.py
+++ algorithm/test.py
@@ -1,31 +1,6 @@
 import unittest
-
+import sol1
 from parameterized import parameterized
-
-
-def solution(n: int) -> int:
-    ant_sequence = make_ant_sequence(n, [1])
-    mid = len(ant_sequence) // 2
-    return ant_sequence[mid - 1] * 10 + ant_sequence[mid]
-
-
-def make_ant_sequence(n: int, ln: list) -> list:
-    if n == 1:
-        return ln
-
-    cur, cnt = ln[0], 1
-    new_ln = []
-    for i in range(1, len(ln)):
-        if cur == ln[i]:
-            cnt += 1
-        else:
-            new_ln.append(cnt)
-            new_ln.append(cur)
-            cur, cnt = ln[i], 1
-    new_ln.append(cnt)
-    new_ln.append(cur)
-
-    return make_ant_sequence(n - 1, new_ln)
 
 
 class AntSequenceTest(unittest.TestCase):
@@ -37,7 +12,7 @@ class AntSequenceTest(unittest.TestCase):
         (8, 21),
     ])
     def test_solution(self, n, result):
-        self.assertEqual(solution(n), result)
+        self.assertEqual(sol1.solution(n), result)
 
     @parameterized.expand([
         (1, [1], [1]),
@@ -50,7 +25,7 @@ class AntSequenceTest(unittest.TestCase):
         (8, [1], [1, 1, 1, 3, 2, 1, 3, 2, 1, 1]),
     ])
     def test_make_ant_sequence(self, n, ln, result):
-        self.assertEqual(make_ant_sequence(n, ln), result)
+        self.assertEqual(sol1.make_ant_sequence(n, ln), result)
 
 
 if __name__ == '__main__':
